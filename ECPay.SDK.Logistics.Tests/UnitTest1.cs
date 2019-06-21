0434f5fdb8f8d0cf82625db839df17ef6760c621
andy840119 <andy840119@gmail.com>
2019-06-21 15:40:01 +0800

    add test project

---
+++ ECPay.SDK.Logistics.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ECPay.SDK.Logistics.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
