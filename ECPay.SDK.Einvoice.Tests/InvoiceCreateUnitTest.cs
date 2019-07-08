6d9adecf99a20b726d97ce8d30d1a64a885127b1
andy840119 <andy840119@gmail.com>
2019-07-08 17:32:45 +0800

    1. rename 2. 把格式換成現代人會寫的那種

--- ECPay.SDK.Einvoice.Tests/InvoiceCreateUnitTest.cs
+++ ECPay.SDK.Einvoice.Tests/InvoiceCreateUnitTest.cs
@@ -15,31 +15,50 @@ namespace ECPay.SDK.Einvoice.Tests
         public void TestInvoiceCreate()
         {
             //1. 設定開立發票資訊
-            InvoiceCreate invc = new InvoiceCreate();
-            invc.MerchantID = "2000132";//廠商編號。
-            invc.RelateNumber = "ecPay" + new Random().Next(0, 99999).ToString();//商家自訂訂單編號
-            invc.CustomerID = "";//客戶代號
-            invc.CustomerIdentifier = "";//統一編號
-            invc.CustomerName = "";//客戶名稱
-            invc.CustomerAddr = "客戶地址";//客戶地址
-            invc.CustomerPhone = "0912345678";//客戶手機號碼
-            invc.CustomerEmail = "test@ecpay.com.tw";//客戶電子信箱
-            //invc.ClearanceMark = CustomsClearanceMarkEnum.None;//通關方式
-            invc.Print = PrintEnum.No;//列印註記
-            invc.Donation = DonationEnum.No;//捐贈註記
-            invc.LoveCode = "";//愛心碼
-            invc.carruerType = CarruerTypeEnum.PhoneBarcode;//載具類別
-            invc.CarruerNum = "/6G+X3LQ";
-            invc.CarruerNum = invc.CarruerNum.Replace('+', ' '); //依API說明,把+號換成空白
-            //invc.TaxType = TaxTypeEnum.DutyFree;//課稅類別
-            invc.SalesAmount = "300";//發票金額。含稅總金額。
-            invc.InvoiceRemark = "(qwrrg)";//備註
-
-            invc.invType = TheWordTypeEnum.Normal;//發票字軌類別
-            //invc.vat = VatEnum.No;//商品單價是否含稅
+            var invoice = new InvoiceCreate
+            {
+                //廠商編號。
+                MerchantID = "2000132",
+                //商家自訂訂單編號
+                RelateNumber = "ecPay" + new Random().Next(0, 99999),
+                //客戶代號
+                CustomerID = "",
+                //統一編號
+                CustomerIdentifier = "",
+                //客戶名稱
+                CustomerName = "",
+                //客戶地址
+                CustomerAddr = "客戶地址",
+                //客戶手機號碼
+                CustomerPhone = "0912345678",
+                //客戶電子信箱
+                CustomerEmail = "test@ecpay.com.tw",
+                //通關方式
+                //ClearanceMark = CustomsClearanceMarkEnum.None;
+                //列印註記
+                Print = PrintEnum.No,
+                //捐贈註記
+                Donation = DonationEnum.No,
+                //愛心碼
+                LoveCode = "",
+                //載具類別
+                carruerType = CarruerTypeEnum.PhoneBarcode,
+                //依API說明,把+號換成空白
+                CarruerNum = "/6G+X3LQ".Replace('+', ' '),
+                //課稅類別
+                //TaxType = TaxTypeEnum.DutyFree;
+                //發票金額。含稅總金額。
+                SalesAmount = "300",
+                //備註
+                InvoiceRemark = "(qwrrg)",
+                //發票字軌類別
+                invType = TheWordTypeEnum.Normal,
+                //商品單價是否含稅
+                //vat = VatEnum.No,
+            };
 
             //2. 商品資訊(Item)的集合類別。
-            invc.Items.Add(new Item
+            invoice.Items.Add(new Item
             {
                 ItemName = "糧食",//商品名稱
                 ItemCount = "1",//商品數量
@@ -48,7 +67,7 @@ namespace ECPay.SDK.Einvoice.Tests
                 //ItemTaxType  =TaxTypeEnum.DutyFree//商品課稅別
                 ItemAmount = "100.1",//總金額
             });
-            invc.Items.Add(new Item
+            invoice.Items.Add(new Item
             {
                 ItemName = "糧食",//商品名稱
                 ItemPrice = "200",//商品單價
@@ -59,19 +78,19 @@ namespace ECPay.SDK.Einvoice.Tests
             });
 
             //3. 執行API的回傳結果
-            var obj = _client.Post<InvoiceCreateReturn, InvoiceCreate>(invc);
+            var response = _client.Post<InvoiceCreateReturn, InvoiceCreate>(invoice);
 
             //表示成功
-            Assert.AreEqual("1", obj.RtnCode);
+            Assert.AreEqual("1", response.RtnCode);
 
             //要有時間
-            Assert.AreNotEqual("", obj.InvoiceDate);
+            Assert.AreNotEqual("", response.InvoiceDate);
 
             //要有號碼
-            Assert.AreNotEqual("", obj.InvoiceNumber);
+            Assert.AreNotEqual("", response.InvoiceNumber);
 
             //要有隨機碼
-            Assert.AreNotEqual("", obj.RandomNumber);
+            Assert.AreNotEqual("", response.RandomNumber);
         }
     }
 }
