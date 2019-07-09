5e2d48ec3d29fb25e46001a5e67fc3b2c6a98750
andy840119 <andy840119@gmail.com>
2019-07-09 17:51:53 +0800

    看不懂在寫什麼 但也只能搬了

---
+++ ECPay.SDK.Payment/WCF/IAllPayService.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+
+namespace ECPay.SDK.Payment.WCF
+{
+    [ServiceContract(Namespace = "http://Payment.allpay.com.tw/")]
+    internal interface IAllPayService
+    {
+        [OperationContract(Name = "QueryTrade", Action = "http://Payment.allpay.com.tw/QueryTrade", ReplyAction = "*")]
+        string QueryTrade(string MerchantID, string XmlData);
+        [OperationContract(Name = "QueryTradeInfo", Action = "http://Payment.allpay.com.tw/QueryTradeInfo", ReplyAction = "*")]
+        string QueryTradeInfo(string MerchantID, string MerchantTradeNo, int TimeStamp, string CheckMacValue);
+    }
+}
